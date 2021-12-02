b153bf1277c1cecf796762890fa73e81809f5f65
Andrey Zadachin <zadachin86@gmail.com>
2021-12-02 10:27:17 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <InputItem @add-item="addItem" />
+    <TodoInProcess :db="db.length" />
+    <TodoList :todoLists="db" @remove-item="removeItem" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//@ - указывает путь от папки src
+import InputItem from '@/components/InputItem.vue';
+import TodoInProcess from '@/components/TodoInProcess.vue';
+import TodoList from '@/components/TodoList.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      db: [],
+    };
+  },
+  //Запрос с сервера
+  mounted() {
+    fetch('https://vue-test-db-bd42b-default-rtdb.firebaseio.com/todo.json')
+      .then((response) => response.json())
+      .then((res) => {
+        this.db = res;
+      })
+      .catch((error) => {
+        console.log('Ошибка ' + error);
+      });
+  },
+  //Регистрируем компоненты
   components: {
-    HelloWorld
-  }
-}
+    InputItem,
+    TodoInProcess,
+    TodoList,
+  },
+  methods: {
+    //Удаляем задачу по id
+    removeItem(id) {
+      this.db = this.db.filter((item) => item.id !== id);
+    },
+    //Добавляем новую задачу
+    addItem(item) {
+      this.db = [...this.db, item];
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,6 +56,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 80%;
+  margin: 0 auto;
   margin-top: 60px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
 }
 </style>
